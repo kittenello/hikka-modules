d88776f5ed9438ed926e13b758975231d52ed7ac
kittenello <marquisyuki10@gmail.com>
2025-06-17 01:00:16 +0300

    Update AutoReaction.py

--- AutoReaction.py
+++ AutoReaction.py
@@ -1,17 +1,18 @@
     # meta developer: @your_username
     # 1
+
     import asyncio
     import uuid
     from telethon.tl.types import Message, ReactionEmoji
     from telethon.tl.functions.messages import SendReactionRequest
     from telethon.errors import ChannelInvalidError, ChannelPrivateError, ChatAdminRequiredError
     from .. import loader, utils
-    
-    
+
+
     @loader.tds
     class AutoReactionsMod(loader.Module):
         """Automatically adds reactions to messages in specified chats from your account"""
-    
+
         strings = {
             "name": "AutoReactions",
             "added": "<b>✅ Auto-reaction added with ID: <code>{}</code>\nChat: <a href='{}'>{}</a>\nReaction: {}</b>",
@@ -25,7 +26,7 @@
             "_cmd_doc_aulist": "Show list of active auto-reactions",
             "_cmd_doc_aud": "[ID] - Delete auto-reaction by ID",
         }
-    
+
         strings_ru = {
             "added": "<b>✅ Автореакция добавлена с ID: <code>{}</code>\nЧат: <a href='{}'>{}</a>\nРеакция: {}</b>",
             "list": "<b>📋 Список автореакций:</b>\n{}",
@@ -38,11 +39,11 @@
             "_cmd_doc_aulist": "Показать список активных автореакций",
             "_cmd_doc_aud": "[ID] - Удалить автореакцию по ID",
         }
-    
+
         def __init__(self):
             self.config = loader.ModuleConfig()
             self._reactions = {}  # {id: {"chat_id": int, "reaction": str}}
-    
+
         async def client_ready(self):
             self._log_chat, _ = await utils.asset_channel(
                 self._client,
@@ -53,15 +54,15 @@
                 _folder="hikka",
             )
             self._log_chat_id = f"-100{self._log_chat.id}"
-    
+
         async def aucmd(self, message: Message):
             """[@chat/ID] [reaction/ID] - Set auto-reaction for a chat"""
             args = utils.get_args_raw(message).split(maxsplit=1)
-    
+
             if len(args) < 2 and not message.is_reply:
                 await utils.answer(message, self.strings("invalid_args"))
                 return
-    
+
             chat_id = args[0] if len(args) >= 1 else None
             try:
                 chat = await self._client.get_entity(chat_id) if chat_id else message.chat
@@ -75,7 +76,7 @@
             except Exception:
                 await utils.answer(message, self.strings("invalid_args"))
                 return
-    
+
             reaction = None
             if message.is_reply:
                 replied = await message.get_reply_message()
@@ -86,37 +87,37 @@
                     return
             else:
                 reaction = args[1]
-    
+
             if not isinstance(reaction, (str, ReactionEmoji)):
                 await utils.answer(message, self.strings("invalid_reaction"))
                 return
-    
+
             reaction_id = str(uuid.uuid4())
             self._reactions[reaction_id] = {
                 "chat_id": chat_id,
                 "reaction": reaction.emoticon if isinstance(reaction, ReactionEmoji) else reaction
             }
-    
+
             self.set("reactions", self._reactions)
-    
+
             reaction_str = reaction.emoticon if isinstance(reaction, ReactionEmoji) else reaction
             await utils.answer(
                 message,
                 self.strings("added").format(reaction_id, chat_link, utils.escape_html(chat_title), reaction_str)
             )
-    
+
             await self.inline.bot.send_message(
                 self._log_chat_id,
                 self.strings("added").format(reaction_id, chat_link, utils.escape_html(chat_title), reaction_str),
                 parse_mode="HTML",
             )
-    
+
         async def aulistcmd(self, message: Message):
             """Show list of active auto-reactions"""
             if not self._reactions:
                 await utils.answer(message, self.strings("no_reactions"))
                 return
-    
+
             response = []
             for rid, data in self._reactions.items():
                 try:
@@ -135,32 +136,32 @@
                         f"💬 Unknown chat (ID: {data['chat_id']})\n"
                         f"😊 {data['reaction']}\n"
                     )
-    
+
             await utils.answer(message, self.strings("list").format("\n".join(response)), parse_mode="HTML")
-    
+
         async def audcmd(self, message: Message):
             """[ID] - Delete auto-reaction by ID"""
             args = utils.get_args_raw(message)
             if not args or args not in self._reactions:
                 await utils.answer(message, self.strings("invalid_args"))
                 return
-    
+
             del self._reactions[args]
             self.set("reactions", self._reactions)
-    
+
             await utils.answer(message, self.strings("deleted").format(args))
-    
+
             await self.inline.bot.send_message(
                 self._log_chat_id,
                 self.strings("deleted").format(args),
                 parse_mode="HTML",
             )
-    
+
         async def watcher(self, message: Message):
             """Watch for messages in chats with auto-reactions"""
             if not self._reactions:
                 return
-    
+
             chat_id = utils.get_chat_id(message)
             for data in self._reactions.values():
                 if data["chat_id"] == chat_id:
